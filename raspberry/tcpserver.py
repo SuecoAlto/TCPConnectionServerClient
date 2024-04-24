591607462b9049795e496b2a8562d4805e50fcb1
suecoalto <suecoalto@gmail.com>
2024-04-25 01:45:35 +0200

    Orginazed the commands in a enum class

--- raspberry/tcpserver.py
+++ raspberry/tcpserver.py
@@ -3,3 +3,14 @@ import struct
 import serial
 import threading
 from enum import Enum
+
+class MessageType(Enum):
+    TURN_ON_GREEN = 1
+    TURN_ON_BLUE = 2
+    TURN_OFF_GREEN = 3
+    TURN_OFF_BLUE = 4
+    ACTUATE_SERVO = 5
+    QUERY_STATUS = 6
+    ACKNOWLEDGE = 7
+    ERROR = 8
+    
