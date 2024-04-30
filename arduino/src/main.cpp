a79bda6400ac12ab2903e94b4cd44cacea8bd985
suecoalto <suecoalto@gmail.com>
2024-04-30 03:05:41 +0200

    Handling Commands
    Implemented some lines to parse incoming strings to match predefined commands. For instance, detect keywords like "on", "off", and servo angle specifications.
    Upon parsing, execute the corresponding actions such as turning LEDs on/off or setting the servo position.
    Send a simple acknowledgment back to the Raspberry Pi, indicating success or failure of the command execution.

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -7,32 +7,57 @@ const int LED_GREEN = 6;
 const int LED_BLUE = 7;
 const int SERVO_PIN = 8;
 
-Servo myServo;                                          // Creates a Servo object that allows you to control a servo motor.
+Servo myServo; // Creates a Servo object that allows you to control a servo motor.
 
 // Initialization code in setup()
 void setup() {
   // Set pin modes
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
-  myServo.attach(SERVO_PIN);                            // Attaches the servo motor to the pin defined by SERVO_PIN.
+  myServo.attach(SERVO_PIN); // Attaches the servo motor to the pin defined by SERVO_PIN.
 
   // Start serial communication if needed
-  Serial.begin(9600);                                   // Initializes serial communication at 9600 bits per second.
+  Serial.begin(9600); // Initializes serial communication at 9600 bits per second.
 }
 
 
 // Main code in loop()
 void loop() {
-  if (Serial.available()) {                             // Checks if there is any incoming data on the serial port.
+  if (Serial.available()) {// Checks if there is any incoming data on the serial port.
     String command = Serial.readStringUntil('\n');
-    handleCommand(command);                             // Calls the handleCommand function with the received command string.
+    Serial.print("Full command receiced: ");
+    Serial.println(command);
+    handleCommand(command); // Calls the handleCommand function with the received command string.
   }
 
 
 // Additional functionality can be added here
 void handleCommand(String command) {
-  // Command parsing logic will be implemented here
-  // This function will contain the logic to parse and execute commands based on the received string.
+  if (command.startsWith("green-on")) {
+    digitalWrite(LED_GREEN, HIGH);
+    Serial.println("GREEN LED ON");
+  } 
+  else if (command.startsWith("blue-on")) {
+    digitalWrite(LED_BLUE, HIGH);
+    Serial.println("BLUE LED ON")
+  }
+  else if (command.startsWith("green-off")) {
+    digitalWrite(LED_GREEN, LOW);
+    Serial.println("GREEN LED OFF");
+  } 
+  else if (command.startsWith("blue-off")) {
+    digitalWrite(LED_BLUE, LOW);
+    Serial.println("BLUE LED OFF");
+  } 
+  else if (command.startsWith("servo-")) {
+    int pos = command.substring(6).toInt();
+    myServo.write(pos);
+    Serial.print("SERVO POSITION SET TO: ");
+    Serial.println(pos);
+  } 
+  else {
+    Serial.println("INVALID COMMAND RECEIVED");
+  }
 }
 
 
